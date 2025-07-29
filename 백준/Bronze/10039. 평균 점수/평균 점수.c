af100aab420648dc4a969548ac7ae86394c5b4f3
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2025-07-29 12:23:18 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void) {
+	int num[5], total=0;
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", &num[i]);
+		if (num[i] < 40) num[i] = 40;
+		total += num[i];
+	}
+	printf("%d", total / 5);
+}

c457492087737bcef8d6fee5e4593ec8e7644a41
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2025-07-29 12:11:07 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b;
+	scanf("%d %d",&a,&b);
+	if (a<=b){
+		printf("%d",(a-1)+(a)*(b-1));
+	}
+	else {
+		printf("%d",(b-1)+(b)*(a-1));
+	}
+	return 0;
+}

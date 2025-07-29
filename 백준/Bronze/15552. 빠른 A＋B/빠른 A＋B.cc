9678b5c9c5403cac81beb6d081ffb5c568ae4179
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2025-07-29 12:29:43 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 356 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main(){
+
+
+
+int a, b, c,d;
+
+scanf("%d", &a);
+
+for (b = 1; b <= a; b++)
+
+{
+
+scanf("%d %d", &c, &d);
+
+printf("%d\n", c + d);
+
+    
+}
+return 0;
+}

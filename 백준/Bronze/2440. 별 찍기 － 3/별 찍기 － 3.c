312a86e8262d1f6993d141047c664e0824d406a8
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2025-07-29 12:11:37 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+   int a,b,c,d;
+   scanf("%d",&a);
+   
+   for(b=1;b<=a;b++){
+   	for(c=a;c>=b;c--){
+   		printf("*");
+   	}
+   	printf("\n");
+   }
+   return 0;
+}

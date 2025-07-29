2cd019c3d96a8652ee6047a58c37db0db58a3e53
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2025-07-29 12:31:08 +0900

    [Bronze V] Title: 余り (Remainder), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int main(void) {
+	
+	int num;
+
+	cin >> num;
+
+	cout << num % 21;
+	
+	return 0;
+}
+

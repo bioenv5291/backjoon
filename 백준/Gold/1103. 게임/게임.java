9326a9c94b71c07876c37c7cdc160c5d30ae40b0
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2025-07-29 13:48:59 +0900

    [Gold II] Title: 게임, Time: 128 ms, Memory: 16624 KB -BaekjoonHub

---
+++ 백준/Gold/1103. 게임/게임.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb= new StringBuilder();
+	static StringTokenizer st;
+	
+	static int n, m, max; // n : 세로, m : 가로 
+	static boolean isCycle = false;
+	static int[][] dp; 
+	static char[][] map;
+	static boolean[][] visited;
+	static int[] moveX = {0, 1, 0, -1};
+	static int[] moveY = {-1, 0, 1, 0};
+	
+	public static void main(String args[]) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		
+		n = Integer.valueOf(st.nextToken());
+		m = Integer.valueOf(st.nextToken());
+		dp = new int[n][m];
+		map = new char[n][m];
+		visited = new boolean[n][m];
+		 
+		for(int i = 0; i < n; i++) {
+			String line = br.readLine();
+			map[i] = line.toCharArray();
+		}
+		
+		visited[0][0] = true;
+		dfs(0, 0, 1);
+		if(isCycle) {
+			System.out.println("-1");
+		}
+		else {
+			System.out.println(max);
+		}
+	}
+	
+	
+	static void dfs(int x, int y, int moveCount) {
+		int moveSquareCount = Character.getNumericValue(map[y][x]);	// X만큼 이동해야할 때, 그 X를 가리킴
+		dp[y][x] = moveCount;
+		if(moveCount > max) {
+			//System.out.println(moveCount + " .. " + y + " , " + x);
+			max = moveCount;
+		}
+		
+		for(int i = 0; i < moveX.length; i++) {
+			int nextX = x + (moveSquareCount * moveX[i]);
+			int nextY = y + (moveSquareCount * moveY[i]);
+			
+			if(nextX < 0 || nextX >= m || nextY < 0 || nextY >= n) {
+				continue;
+			}	
+			
+			if(map[nextY][nextX] == 'H') {	
+				continue;
+			}
+			
+		
+			if(moveCount < dp[nextY][nextX]) { 
+				continue;
+			}
+			
+			if(visited[nextY][nextX]) {
+				isCycle = true;
+				return;
+			}
+
+			visited[nextY][nextX] = true;
+			dfs(nextX, nextY, moveCount + 1);
+			visited[nextY][nextX] = false;
+			
+		}
+	}
+}

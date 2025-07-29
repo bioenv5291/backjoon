4ff899abfdfd2bbe9c346022814d17c07701a331
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2025-07-29 12:23:27 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void) {
+	
+	printf("\\    /\\ \n");
+	printf(" \)  \( '\) \n");
+	printf("\(  \/  \) \n");
+	printf(" \\(__\)| \n");
+	return 0;
+}
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/

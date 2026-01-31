ed0b91e164d5f84105c984c79dc438c7675cf7fc
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2026-01-31 22:23:51 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 64 ms, Memory: 11320 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+
+//#include <bits/stdc++.h>
+using namespace std;
+vector<int> graph[100001];
+int visited[100001] = {0, };
+int result[100001];
+int cnt = 0;
+
+void dfs(int r){
+    if(visited[r]==1) return;
+
+    visited[r] =1;
+    cnt++;
+    result[r] = cnt;
+
+    for(int i=0; i<graph[r].size(); i++) dfs(graph[r][i]);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m, r;
+    cin >> n>> m>> r;
+    for (int i =0; i<m; i++){
+        int a,b;
+        cin >> a>>b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+
+    }
+    for(int i = 1; i<=n; i++){
+        sort(graph[i].begin(), graph[i].end());
+
+    }
+    dfs(r);
+    for(int i =1; i<=n; i++){
+        printf("%d\n", result[i]);
+    }
+    return 0;
+}

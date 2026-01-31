5304e966a8ec424d774068a538854a41cf1b46d7
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2026-01-31 22:06:41 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 64 ms, Memory: 9940 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+
+//#include <bits/stdc++.h>
+using namespace std;
+vector<int> graph[100001];
+int visited[100001] = {0, };
+int result[100001];
+int cnt = 0;
+
+void bfs(int r){
+    queue<int> q;
+    q.push(r);
+    visited[r] = 1;
+    cnt++;
+    result[r] = cnt;
+    while(!q.empty()){
+        int inq = q.front();
+
+        q.pop();
+
+        for(int i = 0; i< graph[inq].size();i++){
+            int temp= graph[inq][i];
+            if(!visited[temp]){
+                cnt++;
+                result[temp]=cnt;
+                q.push(temp);
+                visited[temp]=1;
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m, r;
+    cin >> n>> m>> r;
+    for (int i =0; i<m; i++){
+        int a,b;
+        cin >> a>>b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+
+    }
+    for(int i = 1; i<=n; i++){
+        sort(graph[i].begin(), graph[i].end());
+
+    }
+    bfs(r);
+    for(int i =1; i<=n; i++){
+        printf("%d\n", result[i]);
+    }
+    return 0;
+}

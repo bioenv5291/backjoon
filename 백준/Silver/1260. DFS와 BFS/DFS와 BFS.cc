3b990cd0b2141d91cbea17ff090c2863bfe53217
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2026-01-31 22:57:42 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+
+//#include <bits/stdc++.h>
+using namespace std;
+int N, M, V;
+int graph[1001][1001];
+bool visited[1001];
+queue<int> q;
+
+void reset(){
+    for(int i=1; i<=N; i++){
+        visited[i] = 0;
+    }
+}
+
+void dfs(int v){
+    visited[v] = true;
+    cout << v << " ";
+
+    for(int i=1; i<=N; i++){
+        if(graph[v][i]==1 && visited[i]==0){
+            dfs(i);
+        }
+    }
+}
+
+void bfs(int v){
+    q.push(v);
+    visited[v] = true;
+    cout << v << " ";
+
+    while(!q.empty()){
+        v = q.front();
+        q.pop();
+
+        for(int w=1; w<=N; w++){
+            if(graph[v][w]==1 && visited[w]==0){
+                q.push(w);
+                visited[w] = true;
+                cout << w << " ";
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M >> V;
+    for (int i = 0; i < M; i++){
+        int a, b;
+        cin >> a >> b;
+        graph[a][b] = 1;
+        graph[b][a] = 1;
+    }
+   reset();
+   dfs(V);
+   cout<<'\n';
+
+   reset();
+   bfs(V);
+   
+    return 0;
+}

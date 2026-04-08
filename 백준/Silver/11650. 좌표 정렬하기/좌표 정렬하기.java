2bc357f54b9938ef3803cfcccef638adcabbcd9d
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2026-04-08 22:35:42 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1256 ms, Memory: 57804 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static class Point{
+        int x;
+        int y;
+
+        Point(int x, int y){
+            this.x =x;
+            this.y=y;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        Point[] arr = new Point[num];
+        //Integer.compare(4,2);
+        for (int i = 0; i < num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[i] = new Point(x,y);
+        }
+        Arrays.sort(arr,(Point a,Point b)->{
+            if(a.x == b.x){
+                return Integer.compare(a.y,b.y);
+            }
+            return Integer.compare(a.x,b.x);
+        });
+
+        for(int i=0; i<num; i++)
+            System.out.println(arr[i].x+" "+arr[i].y);
+    }
+    }
